cb99b6e69dc0f0e0b2fa54cfb95d3dc232e75e7e
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-11-03 23:47:42 -0500

    added mentions route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :posts
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :mentions, only: [:index]
 end
