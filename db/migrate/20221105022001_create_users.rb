0ab90f6cd7f2de02d78303c160b206aa286a84b4
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-11-04 21:20:10 -0500

    create user model

---
+++ db/migrate/20221105022001_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.datetime :announcements_last_read_at
+      t.boolean :admin
+
+      t.timestamps
+    end
+  end
+end
