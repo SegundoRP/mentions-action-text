f9c9f561cc48b085a76d4e8d70cfb27dc4f7a7ac
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-11-08 00:09:32 -0500

    added method attachment in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
 
   has_many :notifications, foreign_key: :recipient_id
   has_many :services
+
+  def to_trix_content_attachment_partial_path
+    to_partial_path
+  end
 end
