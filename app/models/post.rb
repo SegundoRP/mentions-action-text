646d3e4ab9ba56278ce89449e6be116de91570e8
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-11-08 23:11:06 -0500

    create mailer

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   has_rich_text :body
+
+  def send_notifications
+    users = user_mentions
+  end
+
+  def user_mentions
+    @users ||= body.body.attachments.select { |a| a.attachable.class == User }.map(&:attachable).uniq
+  end
 end
