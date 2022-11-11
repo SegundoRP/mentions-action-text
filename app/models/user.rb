32b3ee5e320ba7011c8ba6873c1c524ffd01fbe4
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-11-10 22:31:13 -0500

    added render palin text in action text

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,14 @@ class User < ApplicationRecord
   has_many :notifications, foreign_key: :recipient_id
   has_many :services
 
+
+  # this is for html render
   def to_trix_content_attachment_partial_path
     to_partial_path
   end
+
+  # When ActionText rendering mentions in plain text
+  # def attachable_plain_text_representation(caption = nil)
+  #   caption || name
+  # end
 end
