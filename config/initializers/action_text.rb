eddc9fb71e2e6ca17454b075858a7096fe30d3d8
Segundo Rebaza <alvaro.rp1593@gmail.com>
2022-11-10 22:22:02 -0500

    added modifiers to html attributes in action text

---
+++ config/initializers/action_text.rb
@@ -0,0 +1,28 @@
+require "addressable/uri"
+
+module ActionTextNoFollowLinks
+  def to_html
+    fragment.replace("a") do |anchor|
+      href = anchor.attributes["href"].value
+      if allowed_domain?(href)
+        anchor.remove_attribute("rel")
+      else
+        anchor["rel"] = "noopener nofollow"
+      end
+      anchor
+    end.to_html
+  end
+
+  def allowed_domain?(url)
+    ["gorails.com"].include? Addressable::URI.parse(url).domain
+  end
+end
+
+ActiveSupport.on_load :action_text_content do
+  self.prepend ActionTextNoFollowLinks
+end
+
+ActionText::ContentHelper.allowed_attributes += ["rel"]
+
+
+# ayuda a modificar los atributos de los enlaces que se escriban en el rich text
